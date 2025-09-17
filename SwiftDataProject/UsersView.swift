f4224af7c6890bda983cec3ed16c0007865d2c60
d.o.kononov <d.o.kononov@gmail.com>
2025-09-17 11:06:44 +0300

    sort and filter

---
+++ SwiftDataProject/UsersView.swift
@@ -0,0 +1,18 @@
+//
+//  UsersView.swift
+//  SwiftDataProject
+//
+//  Created by Dmitry Kononov on 17.09.25.
+//
+
+import SwiftUI
+
+struct UsersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsersView()
+}
