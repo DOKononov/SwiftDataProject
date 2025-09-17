7c10c1b3a4ed0f880f315b93f30fbd02954ae7ae
d.o.kononov <d.o.kononov@gmail.com>
2025-09-17 09:53:01 +0300

    Query+Predicate

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -10,24 +10,38 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query(sort: \User.name) private var users: [User]
-    @State private var path: [User] = []
+    @Query(filter: #Predicate<User> { user in user.name.contains("3") },
+        sort: \User.name) private var users: [User]
+//    @State private var path: [User] = []
     
     var body: some View {
-        NavigationStack(path: $path) {
+        NavigationStack {
             List(users) { user in
                 NavigationLink(value: user) {
                     Text(user.name)
                 }
             }
-            .navigationDestination(for: User.self) { user in
-                EditeUserView(user: user)
-            }
+//            .navigationDestination(for: User.self) { user in
+//                EditeUserView(user: user)
+//            }
             .toolbar {
-                Button("Add user", systemImage: "plus") {
-                    let user = User(name: "", city: "", joinDate: .now)
-                    modelContext.insert(user)
-                    path = [user]
+                Button("Add samples", systemImage: "plus") {
+//                    let user = User(name: "", city: "", joinDate: .now)
+//                    modelContext.insert(user)
+//                    path = [user]
+                    
+                    try? modelContext.delete(model: User.self)
+                    
+                    let first = User(name: "Test", city: "Test", joinDate: .now.addingTimeInterval(8640 * -10))
+                    let second = User(name: "Test2", city: "Test2", joinDate: .now.addingTimeInterval(8640 * -5))
+                    let third = User(name: "Test3", city: "Test3", joinDate: .now.addingTimeInterval(8640 * 5))
+                    let fourth = User(name: "Test4", city: "Test4", joinDate: .now.addingTimeInterval(8640 * 10))
+                    
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
+                    
                 }
             }
             .navigationTitle("Users")
--- SwiftDataProject/EditeUserView.swift
+++ SwiftDataProject/EditeUserView.swift
@@ -5,23 +5,23 @@
 //  Created by Dmitry Kononov on 16.09.25.
 //
 
-import SwiftUI
-
-struct EditeUserView: View {
-    @Bindable var user: User
-    
-    
-    var body: some View {
-        Form {
-            TextField("User name", text: $user.name)
-            TextField("User city", text: $user.city)
-            DatePicker("Join date", selection: $user.joinDate, displayedComponents: .date)
-        }
-        .navigationTitle("Edite user")
-        .navigationBarTitleDisplayMode(.inline)
-    }
-}
-
-#Preview {
-    EditeUserView(user: User(name: "Test", city: "Test", joinDate: .now))
-}
+//import SwiftUI
+//
+//struct EditeUserView: View {
+//    @Bindable var user: User
+//    
+//    
+//    var body: some View {
+//        Form {
+//            TextField("User name", text: $user.name)
+//            TextField("User city", text: $user.city)
+//            DatePicker("Join date", selection: $user.joinDate, displayedComponents: .date)
+//        }
+//        .navigationTitle("Edite user")
+//        .navigationBarTitleDisplayMode(.inline)
+//    }
+//}
+//
+//#Preview {
+//    EditeUserView(user: User(name: "Test", city: "Test", joinDate: .now))
+//}
